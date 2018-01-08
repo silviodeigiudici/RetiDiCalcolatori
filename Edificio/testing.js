5e2863190e3d11f2e2510eadc1868a6a49593b22
MarcoMorella <marcomorella1@gmail.com>
2018-01-08 15:57:28 +0100

    Changed the logic of Classroom selection URL and the get function for the single building page.

--- Edificio/testing.js
+++ Edificio/testing.js
@@ -11,17 +11,10 @@ app.listen(server_port, () => {
     console.log('Server in ascolto');
     });
 
-app.get("/spv",(req,res) => {
-  //changing those 3 will let you choose how many classrooms,which and what building
-  //var building = 'spv';
+app.get("/edificio",(req,res) => {
+  var building = req.query.edificio;
+  //changing those 2 will let you choose how many and which classrooms
   var num_classes = 3;
   var classes = ["3", "4", "24"];
-  res.render('edificio.ejs', {num_classes :num_classes, classes:classes,building:'spv'});
-});
-app.get("/diag",(req,res) => {
-  //changing those 3 will let you choose how many classrooms,which and what building
-  //var building = 'DIAG';
-  var num_classes = 3;
-  var classes = ["3", "4", "24"];
-  res.render('edificio.ejs', {num_classes :num_classes, classes:classes,building:'DIAG'});
+  res.render('edificio.ejs', {num_classes :num_classes, classes:classes,building:building});
 });
