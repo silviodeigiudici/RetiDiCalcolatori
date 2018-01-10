3fa2952e9730bef5ebffa89d56cc0c2d481b4df5
MarcoMorella <marcomorella1@gmail.com>
2018-01-10 11:42:20 +0100

    Now showing the correct classes on the selection,taking them from the DB.

--- Edificio/testing.js
+++ Edificio/testing.js
@@ -7,7 +7,7 @@ const NodeCouchDb = require('node-couchdb');
 
 // node-couchdb instance with default options
 const couch = new NodeCouchDb();
-
+var classes;
 app.set('view engine', 'ejs'); //set ejs
 
 app.use(express.static('views')); //to use files of web pages
@@ -16,17 +16,30 @@ app.listen(server_port, () => {
     console.log('Server in ascolto');
     });
 
-
 app.get("/edificio",(req,res) => {
   var building = req.query.edificio;
-  //TODO: Retrieve classes names,put an error page if the page does not exists
+
   couch.get('buildings', building).then(({data, headers, status}) => {
 	var build_name = data.name;
  	var num_classes = data.rooms;
     	var lat = data.addresses.lat;
     	var lon = data.addresses.long;
         var address =  data.addresses.address+ "," +data.addresses.house_number + ","+data.addresses.postal_code  +' Rome RM';
-	var classes = ['14','23','32'];
+	fillArray(building,num_classes);
+	console.log("hi"+classes[0]);
   	res.render('edificio.ejs', {building :building,build_name :build_name ,address : address,lat : lat, lon : lon,classes:classes,num_classes :num_classes});})
 }, err => { console.log(err);
 });
+
+
+function fillArray(building,num_classes){
+couch.get(building.toLowerCase()+'classrooms', "_all_docs").then(({data, headers, status}) => {
+	classes = new Array();
+	for(i = 0;i < num_classes;i++){
+	classes.push(data.rows[i].id);
+	console.log(classes[i]);}
+});}
+
+
+
+
