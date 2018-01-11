bb921eda17a40c6ebc4e8d60903a9ca11cb97159
MarcoMorella <marcomorella1@gmail.com>
2018-01-11 16:31:43 +0100

    All DB interactions are now working correctly. Added all infos about the building to the page,taking them from the DB,and formatted the page accordingly.

--- Edificio/testing.js
+++ Edificio/testing.js
@@ -7,7 +7,7 @@ const NodeCouchDb = require('node-couchdb');
 
 // node-couchdb instance with default options
 const couch = new NodeCouchDb();
-var classes;
+
 app.set('view engine', 'ejs'); //set ejs
 
 app.use(express.static('views')); //to use files of web pages
@@ -17,32 +17,24 @@ app.listen(server_port, () => {
     });
 
 app.get("/edificio",(req,res) => {
-  var building = req.query.edificio;
+  var building = req.query.edificio.toLowerCase();
 
-  couch.get('buildings', building).then(({data, headers, status}) => {
+  couch.get('buildingsdb', building).then(({data, headers, status}) => {
 	var build_name = data.name;
- 	var num_classes = data.rooms;
-    	var lat = data.addresses.lat;
-    	var lon = data.addresses.long;
-        var address =  data.addresses.address+ "," +data.addresses.house_number + ","+data.addresses.postal_code  +' Rome RM';
-	fillClasses(building,num_classes);
-	//other informations
-	var shops = data.shops.shop+data.shops.location;
+	var lat = data.addresses.lat;
+	var lon = data.addresses.long;
+	var address =  data.addresses.address+ "," +data.addresses.house_number + ","+data.addresses.postal_code  +' Rome RM';
+
+	var classes = data.rooms;
+	var rooms = data.rooms_number;
+	var shops = data.shops;
 	var vending_machines = data.vending_machines;
-	var fountain = data.drinking_fountain; 
+	var drinking_fountains = data.drinking_fountains;
 	var study_rooms = data.study_rooms;
 	var libraries = data.libraries;
-	var wifi = data.wifi;
-  	res.render('edificio.ejs', {building :building,build_name :build_name ,address : address,lat : lat, lon : lon,classes:classes,num_classes :num_classes,
-	shops : shops, vending_machines:vending_machines,fountain:fountain,study_rooms:study_rooms,libraries:libraries,wifi:wifi});})
+	var wifi = data.wi_fi;
+
+  	res.render('edificio.ejs', {building :building,build_name :build_name ,address : address,lat : lat, lon : lon,classes:classes,num_classes :classes.length,rooms : rooms,
+	shops : shops, vending_machines:vending_machines,drinking_fountains:drinking_fountains,study_rooms:study_rooms,libraries:libraries,wifi:wifi,});})
 }, err => { console.log(err);
 });
-
-
-function fillClasses(building,num_classes){
-couch.get(building.toLowerCase()+'classrooms', "_all_docs").then(({data, headers, status}) => {
-	classes = new Array();
-	for(i = 0;i < num_classes;i++){
-	classes.push(data.rows[i].id);
-	console.log(classes[i]);}
-});}
