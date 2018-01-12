368594e35aea189355fc4b869a7c8ebd1a14d53c
Nick1296 <mattianicolella@gmail.com>
2018-01-12 11:15:47 +0100

    removed client secret once again

--- Login/code/OAuth_Secret.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-  'google' : {
-        'clientID'      : '122787374629-kf6mbthrq8p5oji3vcgcoi10nh21215d.apps.googleusercontent.com',
-        'clientSecret'  : 'HWn0Udq8pcaAmKrLEOc0cMdh',
-        'callbackURL'   : 'http://localhost:3000/auth'
-    }
-};
